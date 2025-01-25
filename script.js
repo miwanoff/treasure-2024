dc7da90708d6a3e1304a9958a542452240a18543
miwanoff <marina.iva.noffa@gmail.com>
2025-01-25 15:03:29 +0200

    Add Math.abs

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
-let treasure = {
+const treasure = {
   x: 100,
   y: 200,
 };
 
 const map = document.getElementById("map");
 
-map.onmousemove = showCoords;
-
-function showCoords(event) {
-  let coords = document.getElementById("coords");
-  let distanceX = event.offsetX - treasure.x;
-  let distanceY = event.offsetY - treasure.y;
+map.onmousemove = function (event) {
+  const coords = document.getElementById("coords");
+  let distanceX = Math.abs(event.offsetX - treasure.x);
+  let distanceY = Math.abs(event.offsetY - treasure.y);
   let x = event.offsetX;
   //... // тут отримуємо значення координат миші x та y від події event
-  coords.value = "Координати: " + x;
-  if (Math.abs(distanceX) < 30) {
+  coords.value = `Координати: ${x}`;
+  if (distanceX < 30) {
     // умова, де знаходиться скарб - відстань від координат курсору до координат скарбу менше 30
     alert("Скарб тут!");
   }
-}
+};
