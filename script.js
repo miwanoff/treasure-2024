970bb757b15f90d0cee7e03ffc173d74e45f9ed0
miwanoff <marina.iva.noffa@gmail.com>
2025-01-25 15:47:44 +0200

    Add treasureCircle

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ const treasure = {
 };
 
 const map = document.getElementById("map");
+console.log(map);
 console.log(map.width);
 console.log(map.height);
 const wrap = document.getElementById("wrap");
+
+wrap.style.width = `${map.width}px`;
+wrap.style.height = `${map.height}px`;
+wrap.style.position = "relative";
 
 map.onmousemove = function (event) {
   const coords = document.getElementById("coords");
@@ -15,8 +20,11 @@ map.onmousemove = function (event) {
   let x = event.offsetX;
   //... // тут отримуємо значення координат миші x та y від події event
   coords.value = `Координати: ${x}`;
-  if (distanceX < 30) {
+  if (distanceX < 60) {
     // умова, де знаходиться скарб - відстань від координат курсору до координат скарбу менше 30
-    alert("Скарб тут!");
+    let treasureCircle = document.createElement('div');
+    treasureCircle.style = "width: 60px; height: 60px; position: absolute; top: 0; left: 0; border: solid red 3px;";
+    wrap.appendChild(treasureCircle);
+    //alert("Скарб тут!");
   }
 };
